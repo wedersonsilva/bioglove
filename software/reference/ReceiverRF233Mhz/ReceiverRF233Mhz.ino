a1a05a96f29c9967c7f7bdaa372f2c6f23278748
Wederson Silva <wederson18@gmail.com>
2018-06-23 01:33:29 -0300

    v0.1.18 Organiza e diminui o código ao mínimo

--- software/reference/ReceiverRF233Mhz/ReceiverRF233Mhz.ino
+++ software/reference/ReceiverRF233Mhz/ReceiverRF233Mhz.ino
@@ -1,75 +1,46 @@
 /*
-  Programa : Módulo RF Transmissor com Arduino Uno
-  Autor : FILIPEFLOP - Arduino e Cia
+  Programa: Módulo RF 413 Mhz Transmissor 
  
-  Referência:
-  https://www.filipeflop.com/blog/modulo-rf-transmissor-receptor-433mhz-arduino/
+  Referências:
+  https://www.pjrc.com/teensy/td_libs_VirtualWire.html
 
   Modificado por: Wederson Silva
   
-  v0.1.7 Testa o envio de numeros variaveis
+  v0.1.18 Organiza e diminui o código ao mínimo
 */
 
-//Programa : Receptor RF com Arduino Mega e Display Nokia 5110
-//Autor : Arduino e Cia
-
-//#include <Adafruit_GFX.h>      //Carrega a biblioteca do display
-//#include <Adafruit_PCD8544.h>  //Carrega a biblioteca grafica
 #include <VirtualWire.h>
 
 byte message[VW_MAX_MESSAGE_LEN];    // Armazena as mensagens recebidas
 byte msgLength = VW_MAX_MESSAGE_LEN; // Armazena o tamanho das mensagens
+int valor = 0;
 
-// Pinagem do LCD :
-// pin 8 - Serial clock out (SCLK)
-// pin 9 - Serial data out (DIN)
-// pin 10 - Data/Command select (D/C)
-// pin 11 - LCD chip select (CS/CE)
-// pin 12 - LCD reset (RST)
-//Adafruit_PCD8544 display = Adafruit_PCD8544(8, 9, 10, 11, 12); 
-//Inicializa e seta os parametros do display
-
-void setup()   {
+void setup()
+{
   Serial.begin(9600);
-    vw_set_rx_pin(4); // Define o pino 4 do Arduino como entrada 
-//de dados do receptor
-    vw_setup(2000);             // Bits por segundo
-    vw_rx_start();              // Inicializa o receptor
-
-  //display.begin();
-  //display.setContrast(50); //Ajusta o contraste do display
-  //display.clearDisplay();   //Apaga o buffer e o display
-  //display.setTextSize(1);  //Seta o tamanho do texto
-  //display.setTextColor(BLACK); //Seta a cor do texto
-  //display.setCursor(0,0);  //Seta a posição do cursor
-  //display.print("Aguardando...");
-  //display.display();
+  vw_set_rx_pin(4);                 // Pino de dados do receptor
+  vw_setup(2000);                   // Bits por segundo
+  vw_rx_start();                    // Inicializa o receptor
+     
   delay(2000);
-
 }
+
 
 void loop()
 {
-uint8_t message[VW_MAX_MESSAGE_LEN];    
-uint8_t msgLength = VW_MAX_MESSAGE_LEN; 
+  uint8_t message[VW_MAX_MESSAGE_LEN];    
+  uint8_t msgLength = VW_MAX_MESSAGE_LEN; 
   vw_wait_rx();
-  //display.setCursor(0, 0);  //Seta a posição do cursor
-    if (vw_get_message(message, &msgLength)) // Non-blocking
-    {               
-        Serial.print("Recebido: ");
-        
-        
-
-       // display.clearDisplay();   //Apaga o buffer e o display
-        for (int i = 0; i < msgLength; i++)
-       {
-          Serial.write(message[i]);
-        //  display.write(message[i]);
-          //display.display();
-          //Serial.write('message');
-
-          
-       }
-    Serial.println();
+    
+  if (vw_get_message(message, &msgLength)) // Non-blocking
+  {                   
+    Serial.print("Recebido: ");
+               
+    for (int i = 0; i < msgLength; i++)
+    {            
+      Serial.write(message[i]);      
     }
+    
+    Serial.println();
+  }
 }
